aeced6dfdb8bb4f11b0d3ab758778dfe827db97a
raamadithyan <chitha.don@gmail.com>
2024-02-25 23:50:24 +0530

    added tagline

--- app/_components/Heading.tsx
+++ app/_components/Heading.tsx
@@ -4,7 +4,7 @@ function Heading() {
 	console.log("am i client");
 
 	return (
-		<div className="relative">
+		<div className="relative py-10">
 			<div className="drop absolute left-[39%] top-32 -z-10 "></div>
 			<header className="  font-semibold text-[16rem] text-center ">
 				NOTES
